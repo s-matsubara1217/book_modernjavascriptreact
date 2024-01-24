65cdf1cc115c008593415f5068406dfee5bae6ea
s-matsubara <shohs12171217@gmail.com>
2024-01-24 16:52:23 +0900

    chapter4 Reactの基本 終了

---
+++ textbook/chapter4/react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { ColoredMessage } from "./components/ColoredMessage";
+
+export const App = () => {
+  const [num, setNum] = useState(0);
+  const onClickButton = () => {
+    setNum((prev) => prev + 1);
+  }
+
+  useEffect(() => {
+    alert();
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+      <ColoredMessage color="pink">元気です</ColoredMessage>
+      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+    </>
+  );
+}
